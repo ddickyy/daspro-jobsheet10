7a70db9f2dedd297c8d774c758394ef3a9ceed4f
ddickyy <dikiidarmawam05x2@gmail.com>
2024-11-06 08:46:58 +0700

    Setelah soal

--- Bioskop08.java
+++ Bioskop08.java
@@ -10,7 +10,14 @@ public class Bioskop08 {
         penonton [2][0] = "Eka";
         penonton [2][1] = "Farhan";
         penonton [3][0] = "Gisel";
+        penonton [3][1] = "Hana";
 
+        System.out.println("Penonton baris ke-3: ");
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
+        }
+
+        System.out.println();
         System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
